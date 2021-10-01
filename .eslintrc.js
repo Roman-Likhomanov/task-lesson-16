64d8ae2a8f3d24b9e3031a22b2d7d1ec037bd010
Roman Likhomanov <romanlee87@gmail.com>
2021-10-01 16:55:26 +0300

    Installing Packages

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["jest"],
+  rules: {
+    "no-unused-vars": "off",
+    "no-proto": "off",
+    "no-new": "off",
+    "import/extensions": "off",
+    "no-shadow": "off",
+    "no-return-await": "off",
+    "no-undef": "off",
+    "func-names": "off",
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "default-case": "off",
+    "no-useless-escape": "off",
+    "no-alert": "off",
+    "no-console": "off",
+    "max-len": "off",
+    "import/prefer-default-export": "off",
+  },
+};
